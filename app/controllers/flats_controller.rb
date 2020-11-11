6fd0b2c03f5fac6f8f1d2b4f7479e439e7aca8e5
Maxime Lapraye <maxime.lapraye@gmail.com>
2020-11-11 17:03:51 -0500

    airbnb controller flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,22 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Small and Dark',
+  address: '10 nowhere road',
+  description: 'Coucou Poutine Lorem Ipsum',
+  price_per_night: 150,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Spacious Rooftop Flat Madrid',
+  address: '10 Clifton Gardens Madrid W9 1DT',
+  description: 'A lovely summer feel for this spacious rooftop flat. ',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Convenient Paris',
+  address: '10 Paris Street, Paris',
+  description: 'Baguette',
+  number_of_guests: 10
+)
